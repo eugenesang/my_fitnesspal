35d4ab3440e55c4ed5cfa335f8aa06614a67ab37
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-22 20:00:08 +0300

    route exercises

---
+++ server/routes/exercise.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const exerciseData = require('../data').data;
+
+const router = express.Router();
+
+router.get('/bodyPart/:bodyPart', (req, res) => {
+    try {
+        const data = exerciseData[req.params.bodyPart];
+
+        if (!data) {
+            return res.status(404).json({
+                error: "body part not found"
+            })
+        }
+
+        return res.json(data);
+    } catch (error) {
+        res.status(500).json({error});
+        console.error(error);
+    }
+})
+
+module.exports = router;
